f67699ee480a65035e6b47efe224cf90696f6fb8
Yuri Cavalini <yuricavalini@gmail.com>
2022-05-25 20:42:11 -0300

    initial webpack config

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ICarros | ReactJS | Gama Academy
+
+## Para gerenciar a versão do NODEJS use o NVM:
+
+[Clique aqui para acessar documentação](https://github.com/nvm-sh/nvm)
---
+++ webpack.common.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main-bundle-[hash].js',
+    publicPath: '/',
+  },
+  resolve: {
+    extension: ['.js', '.jsx', '.scss'],
+    alias: {
+      '@': path.join(__dirname, 'src'),
+    },
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
